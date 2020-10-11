71f46d8d6ce9f16e57f3e154b4a57ac5b5024e48
Edo <iam@edobarnas.com>
2020-10-11 07:59:49 +0200

    Remove obsolete DTO

--- src/QueryBus.php
+++ src/QueryBus.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace Tuzex\Cqrs;
 
-use Tuzex\Cqrs\Transfer\Dto;
-
 interface QueryBus
 {
-    public function execute(Query $query): Dto;
+    public function execute(Query $query): object;
 }
