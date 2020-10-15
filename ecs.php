6f314377b58284dd101ea1cb9e270dd7b75c3cd6
Edo Barnáš <eduard.barnas@codea.eu>
2020-10-15 13:27:22 +0200

    Remove obsolete directory from CS configuration

--- ecs.php
+++ ecs.php
@@ -12,7 +12,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
     $parameters->set(Option::PATHS, [
         __DIR__ . '/src',
-        __DIR__ . '/tests',
     ]);
 
     $parameters->set(Option::SETS, [
