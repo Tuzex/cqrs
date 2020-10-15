8421e76db26b725b833123326ff89f5382336977
Edo Barnáš <eduard.barnas@codea.eu>
2020-10-15 10:10:55 +0200

    Add exceptions for missing handlers

---
+++ src/Exception/CommandHandlerNotFoundException.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tuzex\Cqrs\Exception;
+
+use LogicException;
+use Throwable;
+use Tuzex\Cqrs\Command;
+
+final class CommandHandlerNotFoundException extends LogicException
+{
+    public function __construct(Command $command, Throwable $previous)
+    {
+        /**
+         * @todo PHP8 => $command::class
+         */
+        parent::__construct(sprintf('Handler for command "%s" not found.', get_class($command)), $previous->getCode(), $previous);
+    }
+}
---
+++ src/Exception/QueryHandlerNotFoundException.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tuzex\Cqrs\Exception;
+
+use LogicException;
+use Throwable;
+use Tuzex\Cqrs\Query;
+
+final class QueryHandlerNotFoundException extends LogicException
+{
+    public function __construct(Query $query, Throwable $previous)
+    {
+        /**
+         * @todo PHP8 => $query::class
+         */
+        parent::__construct(sprintf('Handler for query "%s" not found.', get_class($query)), $previous->getCode(), $previous);
+    }
+}
