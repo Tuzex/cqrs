d6ee52fa408d785d64beaca1cde037d22ff10608
Edo Barnáš <eduard.barnas@codea.eu>
2020-10-27 13:23:32 +0100

    Prepare the environment for unit testing

--- ecs.php
+++ ecs.php
@@ -5,13 +5,14 @@ declare(strict_types=1);
 use SlevomatCodingStandard\Sniffs\TypeHints\PropertyTypeHintSniff;
 use SlevomatCodingStandard\Sniffs\TypeHints\ReturnTypeHintSniff;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symplify\EasyCodingStandard\Configuration\Option;
+use Symplify\EasyCodingStandard\ValueObject\Option;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
     $parameters->set(Option::PATHS, [
         __DIR__ . '/src',
+        __DIR__ . '/tests',
     ]);
 
     $parameters->set(Option::SETS, [
